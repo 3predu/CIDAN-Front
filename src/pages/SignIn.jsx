71a16df014784179b2fb99640c9934b64c508853
Pedro Lucca <3pedrolucca@gmail.com>
2023-06-29 23:19:31 -0300

    add tailwind

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -10,62 +10,18 @@ import {
     Link
 } from "@mui/material";
 
-import styles from "../style/SignIn.module.css";
-
 export default function SignIn() {
     return (
-        <Box
-            sx={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-            }}
-        >
-            <Box
-                sx={{
-                    width: "50%",
-                    height: "100%"
-                }}
-            >
-                <Container
-                    sx={{
-                        width: "85%",
-                        height: "100%",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center"
-                    }}
-                >
-                    <Typography
-                        variant="h4"
-                    >
-                        Entrar
-                    </Typography>
+        <Box>
+            <main>
+                <Box>
+                    <Box>
+                        
+                    </Box>
+                </Box>
 
-                    <Typography
-                        variant="caption"
-                        color="rgb(108, 115, 127)"
-                    >
-                        Não tem uma conta? <Link underline="hover">Criar</Link>
-                    </Typography>
-                </Container>
-            </Box>
-
-            <Box
-                sx={{
-                    width: "50%",
-                    height: "100%",
-                }}
-            >
-                <img
-                    src="IMG_2593.jpg"
-                    style={{
-                        width: "100%",
-                        height: "100%"
-                    }}
-                    alt=""
-                />
-            </Box>
+                <Box></Box>
+            </main>
         </Box>
     );
 }
