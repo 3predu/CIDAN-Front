ec0d9f4fe8fea83888f63d08cef98dd053b0eab2
Pedro Lucca <3pedrolucca@gmail.com>
2023-09-01 16:19:32 -0300

    Add Components - LeftBar

---
+++ src/components/LeftBar.jsx
@@ -0,0 +1,59 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+} from "@mui/material";
+import stylesDashboard from "../style/Dashboard.module.css";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/People";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useNavigate } from "react-router-dom";
+
+export default function LeftBar() {
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        width: "280px",
+        height: "100%",
+        backgroundColor: "#d4e9e2",
+        boxShadow: "4",
+      }}
+    >
+      <Box className={stylesDashboard.Lista}>
+        <img
+          src="./images/logo-cdan.png"
+          style={{ width: "80px", height: "80px", margin: "20px 0" }}
+        />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => navigate("/dashboard")}>
+              <ListItemIcon>
+                <HomeIcon style={{ color: "#1e3932" }} />
+              </ListItemIcon>
+              <ListItemText primary="Início" />
+            </ListItemButton>
+          </ListItem>
+          <ListItemButton onClick={() => navigate("/unities")}>
+            <ListItemIcon>
+              <PeopleIcon style={{ color: "#1e3932" }} />
+            </ListItemIcon>
+            <ListItemText primary="Unidades" />
+          </ListItemButton>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <LogoutIcon style={{ color: "#1e3932" }} />
+              </ListItemIcon>
+              <ListItemText primary="Sair" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </Box>
+  );
+}
